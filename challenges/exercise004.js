5502f80b41d0db788147b01aa562948f8e4bb5a2
Nigel Faria <nigel.faria@gmail.com>
2022-11-29 16:53:11 +0000

    Ex 4 with 'for loop'

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,41 +1,90 @@
 export function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let smallArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 1) {
+      smallArr.push(nums[i]);
+    }
+  }
+  return smallArr;
 }
 
 export function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let nameArr = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].startsWith(char)) {
+      nameArr.push(names[i]);
+    }
+  }
+  return nameArr;
 }
 
 export function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let retArr = [];
+  const starts = "to" + " ";
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].startsWith(starts)) {
+      retArr.push(words[i]);
+    }
+  }
+  return retArr;
 }
 
 export function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let retArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (Number.isInteger(nums[i])) {
+      retArr.push(nums[i]);
+    }
+  }
+  return retArr;
 }
 
 export function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let retArr = []
+  for (let j = 0; j < users.length; j++) {
+    retArr.push(users[j].data.city.displayName)
+  }
+  return retArr;
 }
 
 export function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let retArr = [];
+  for (let k = 0; k < nums.length; k++) {
+    retArr.push(Math.round(Math.sqrt(nums[k]) * 100) / 100);
+  }
+  return retArr;
 }
 
 export function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  let retArr = [];
+  for (let l = 0; l < sentences.length; l++) {
+    if (sentences[l].toLowerCase().includes(str.toLowerCase())) {
+      retArr.push(sentences[l]);
+    }
+  }
+  return retArr;
 }
 
 export function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  let retArr = [];
+  for (let i = 0; i < triangles.length; i++) {
+    let max = triangles[i][0];
+    for (let j = 0; j < triangles[i].length; j++) {
+      if (triangles[i][j] > max) {
+        max = triangles[i][j];
+      }
+    }
+    retArr.push(max);
+  }
+  return retArr;
 }
